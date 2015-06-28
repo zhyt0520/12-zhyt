126f1ee6412221cc2b4039c454530b5eefe58d9a
zhyt <zhyt0520@gmail.com>
2015-06-28 14:48:17 +0800

    just modify indentation a little

--- page-templates/allposts.php
+++ page-templates/allposts.php
@@ -13,25 +13,26 @@ get_header(); ?>
 				<?//php get_template_part( 'content', 'page' ); ?>
 <div id="allpost">
 <table>
-<tr>
-<td class="allpsot-td"><strong>序号</strong></td>
-<td class="allpsot-td"><strong>发布日期</strong></td>
-<td><strong>文章标题</strong></td>
-</tr>
-<?php $count_posts = wp_count_posts(); $published_posts = $count_posts->publish; query_posts('posts_per_page=-1' );
-while ( have_posts() ) : the_post();
-echo '<tr>';
-
-echo '<td class="allpsot-td" width="40">'.$published_posts.'</td>';
-echo '<td  class="allpsot-td" width="130">'; the_time(get_option( 'date_format' ));
-
-echo '</td>
-<td><a href="'; the_permalink();
-echo '" title="'.esc_attr( get_the_title() ).'">'; the_title();
-echo '</a></td>
-
-</tr>'; $published_posts--;
-endwhile; wp_reset_query(); ?>
+	<tr>
+		<td class="allpsot-td"><strong>序号</strong></td>
+		<td class="allpsot-td"><strong>发布日期</strong></td>
+		<td><strong>文章标题</strong></td>
+	</tr>
+	<?php $count_posts = wp_count_posts(); 
+	$published_posts = $count_posts->publish; 
+	query_posts('posts_per_page=-1' );
+	while ( have_posts() ) : the_post();
+		echo '<tr>';
+		echo '<td class="allpsot-td" width="40">'.$published_posts.'</td>';
+		echo '<td  class="allpsot-td" width="130">'; the_time(get_option( 'date_format' ));
+		echo '</td>
+		<td><a href="'; the_permalink();
+		echo '" title="'.esc_attr( get_the_title() ).'">'; the_title();
+		echo '</a></td>
+		</tr>'; 
+		$published_posts--;
+	endwhile; 
+	wp_reset_query(); ?>
 </table>
 </div>
 <br />
