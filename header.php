53c483ef4b85e73359f0ae6d663ff5d59c3b7706
zhyt <zhyt0520@gmail.com>
2017-01-28 02:47:09 +0800

    Update header.php

--- header.php
+++ header.php
@@ -38,7 +38,6 @@
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<p style="float:right; font-size:14px;">切换至：<a href="?am_force_theme_layout=mobile">简版</a></p>
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
