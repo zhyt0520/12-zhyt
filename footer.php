7e3495db7cefb3dfcc07cc4b9fd63470c922d48d
zhyt0520 <zhyt0520@gmail.com>
2017-02-02 02:42:35 +0800

    注释掉标签云浮动的js

--- footer.php
+++ footer.php
@@ -14,14 +14,14 @@
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
 			<p style="display:inline;">Copyright &copy 2013-2017 <a href="http://aword.net">一语-aword</a></p>
-			<p style="display:inline;margin-left:14px;">推荐 <a href="https://my.laoxuehost.net/aff.php?aff=1844" target="_blank">老薛主机</a></p>
+			<p style="display:inline;margin-left:20px;">推荐 <a href="https://my.laoxuehost.net/aff.php?aff=1844" target="_blank">老薛主机</a></p>
 			<p style="display:inline;float:right;margin-left:14px;"><a style="cursor:pointer; "onclick="window.scroll(0,0)">回顶部↑</a></p>
 			<!-- <p style="display:inline;float:right;"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5165710'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s6.cnzz.com/stat.php%3Fid%3D5165710%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script></p> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+<!-- <script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
 <script>
 	$.fn.smartFloat=function(x){
 		var element=$(this);
@@ -41,7 +41,7 @@
 		});
 	};
 	$('#tag_cloud-2').smartFloat(355-24);
-</script>
+</script> -->
 
 <?php wp_footer(); ?>
 </body>
